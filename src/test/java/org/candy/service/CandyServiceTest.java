feb5691320c9e390bc7fe7ee866c643ad51b28d8
Wakana <83waksug@gafe.molndal.se>
2024-02-19 11:32:52 +0100

    error run test

--- src/test/java/org/candy/service/CandyServiceTest.java
+++ src/test/java/org/candy/service/CandyServiceTest.java
@@ -9,10 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
