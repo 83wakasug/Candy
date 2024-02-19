a0a0a73c868e4e8a722107609c4bc8feca4557e7
Wakana <83waksug@gafe.molndal.se>
2024-02-19 13:17:30 +0100

    updated classes

--- src/main/java/org/candy/repository/CandyRepository.java
+++ src/main/java/org/candy/repository/CandyRepository.java
@@ -2,7 +2,9 @@ package org.candy.repository;
 
 import org.candy.entity.Candy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CandyRepository extends JpaRepository<Candy,Long> {
 
     @Override
