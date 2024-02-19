70f3ff8cff574608578d05f71f134ce713dfe127
Wakana <83waksug@gafe.molndal.se>
2024-02-19 14:16:30 +0100

    byte pom

--- src/test/java/org/candy/service/CandyServiceTest.java
+++ src/test/java/org/candy/service/CandyServiceTest.java
@@ -1,7 +1,6 @@
 package org.candy.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.candy.CandyApplication;
 import org.candy.entity.Candy;
 import org.candy.repository.CandyRepository;
 import org.junit.jupiter.api.Test;
