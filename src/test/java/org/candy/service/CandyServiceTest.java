014ecf23927472431e556264071e5e8e59124589
Wakana <83waksug@gafe.molndal.se>
2024-02-19 13:26:39 +0100

    added class path

--- src/test/java/org/candy/service/CandyServiceTest.java
+++ src/test/java/org/candy/service/CandyServiceTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest(classes = CandyServiceTest.class)
+@SpringBootTest(classes = CandyApplication.class)
 @ActiveProfiles("test")
 class CandyServiceTest {
 
