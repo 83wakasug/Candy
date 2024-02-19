6d0d2f1e3a5ea53b426422f7bd74b00c21b79ca0
Wakana <83waksug@gafe.molndal.se>
2024-02-19 13:50:59 +0100

    tog bort active profile

--- src/test/java/org/candy/service/CandyServiceTest.java
+++ src/test/java/org/candy/service/CandyServiceTest.java
@@ -16,8 +16,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-
-@SpringBootTest
+@ActiveProfiles("test")
+@SpringBootTest(classes = CandyApplication.class)
 class CandyServiceTest {
 
     @Mock
