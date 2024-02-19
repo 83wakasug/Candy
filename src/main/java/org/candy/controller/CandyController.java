94a2c01174cac817f5eaf980343208cc3003d261
Wakana <83waksug@gafe.molndal.se>
2024-02-19 08:30:00 +0100

    Control class deleted

--- src/main/java/org/candy/controller/CandyController.java
+++ src/main/java/org/candy/controller/CandyController.java
@@ -21,7 +21,7 @@ private final CandyService service;
     @GetMapping("/")
     public ResponseEntity<?> getCandyInfo(@RequestParam long id){
         try{
-            if (id < 0 || id > Long.MAX_VALUE) {
+            if (id < 0) {
                 return ResponseEntity.badRequest().build();
             }
 
@@ -45,13 +45,61 @@ private final CandyService service;
             Candy addedCandy = service.addCandy(candy);
             return ResponseEntity.ok(addedCandy);
         } catch (DataIntegrityViolationException e) {
-
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         } catch (Exception e) {
 
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-
         }
+
+
+    }
+
+    @PostMapping("/all")
+    public ResponseEntity<?> GetCandyList(Candy candy) {
+        try {
+            Candy addedCandy = service.addCandy(candy);
+            return ResponseEntity.ok(addedCandy);
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } catch (Exception e) {
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+
+    }
+
+    @PutMapping("")
+    public ResponseEntity<?> updateCandy(Candy candy) {
+        try {
+            Candy addedCandy = service.addCandy(candy);
+            return ResponseEntity.ok(addedCandy);
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } catch (Exception e) {
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+
+    }
+
+    @DeleteMapping("")
+    public ResponseEntity<?> deleteCandy(Long id) {
+        try {
+            boolean deleted = service.deleteCandy(id);
+            if(!deleted){
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(deleted);
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } catch (Exception e) {
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+
     }
 
 
